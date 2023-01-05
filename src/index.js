d5b0fdf7f44075d69339e610b10e6940550a7217
AlexsIntroductions <alexsintroductions@gmail.com>
2023-01-05 00:09:42 -0500

    Added Basic Functionality

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Postcard } from './Postcard';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class Tabletop extends React.Component {
+  render() {
+    //<Postcard type={"picture"} in={false} url={"../images/cats.jpg"} content={"A picture of my Cats"} />
+    return (
+      <>
+        <div style={{height: "50%", color: "white", fontSize: "500px" }}>test</div>
+        <div style={{height: "50%", color: "white", fontSize: "500px" }}>test</div>
+        <div style={{height: "50%", color: "white", fontSize: "500px" }}>test</div>
+        <div style={{height: "50%", color: "white", fontSize: "500px" }}>test</div>
+        <div style={{height: "50%", color: "white", fontSize: "500px" }}>test</div>
+        <div style={{height: "50%", color: "white", fontSize: "500px" }}>test</div>
+        <Postcard type={"paragraph"} in={false} url={""} content={"The fitness graham pacer test is a multi stage capacity test that prograssively gets more difficult as it continues. The fitness graham pacer test will begin in 30 seconds so line up at the door this is a continuation of the test"} />
+      </>
+    );
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = createRoot(document.getElementById('root'));
+root.render(<Tabletop />);
